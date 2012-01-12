30d4d1388af2920c571688867352034d65ce6bb2
Tomer Filiba <tomerfiliba@gmail.com>
2012-01-12 14:01:52 +0200

    java

--- dbproj/src/pony/gui/Gui.java
+++ dbproj/src/pony/gui/Gui.java
@@ -51,8 +51,7 @@ public class Gui {
 		shlPonyTrivia.setImage(null);
 		shlPonyTrivia.setSize(647, 373);
 		shlPonyTrivia.setText("Pony Trivia");
-		shlPonyTrivia.setLayout(new GridLayout(2, false));
-		new Label(shlPonyTrivia, SWT.NONE);
+		shlPonyTrivia.setLayout(new GridLayout(1, false));
 		new Label(shlPonyTrivia, SWT.NONE);
 		
 		Button btnPlayGame = new Button(shlPonyTrivia, SWT.NONE);
@@ -61,25 +60,24 @@ public class Gui {
 			public void widgetSelected(SelectionEvent arg0) {
 			}
 		});
-		GridData gd_btnPlayGame = new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1);
+		GridData gd_btnPlayGame = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
 		gd_btnPlayGame.heightHint = 94;
-		gd_btnPlayGame.widthHint = 207;
+		gd_btnPlayGame.widthHint = 248;
 		btnPlayGame.setLayoutData(gd_btnPlayGame);
 		btnPlayGame.setImage(SWTResourceManager.getImage(Gui.class, "/pony/gui/kitty.gif"));
 		btnPlayGame.setText("Play Game");
-		new Label(shlPonyTrivia, SWT.NONE);
 		
 		Button btnImportImdbFiles = new Button(shlPonyTrivia, SWT.NONE);
 		GridData gd_btnImportImdbFiles = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
 		gd_btnImportImdbFiles.heightHint = 71;
-		gd_btnImportImdbFiles.widthHint = 208;
+		gd_btnImportImdbFiles.widthHint = 247;
 		btnImportImdbFiles.setLayoutData(gd_btnImportImdbFiles);
 		btnImportImdbFiles.setImage(SWTResourceManager.getImage(Gui.class, "/pony/gui/IMDB-logo.gif"));
 		btnImportImdbFiles.setText("Import IMDB Files");
 		
 		Button btnEditDb = new Button(shlPonyTrivia, SWT.NONE);
 		GridData gd_btnEditDb = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
-		gd_btnEditDb.widthHint = 208;
+		gd_btnEditDb.widthHint = 248;
 		btnEditDb.setLayoutData(gd_btnEditDb);
 		btnEditDb.setImage(SWTResourceManager.getImage(Gui.class, "/pony/gui/mysql-logo.gif"));
 		btnEditDb.setText("Edit DB");
