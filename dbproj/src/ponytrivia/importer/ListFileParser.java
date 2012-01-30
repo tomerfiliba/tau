b93071c10ab99fe88e600c991a04e9e17be09762
Tomer Filiba <tomerfiliba@gmail.com>
2012-01-30 11:30:49 +0200

    matan's changes

--- dbproj/src/ponytrivia/importer/ListFileParser.java
+++ dbproj/src/ponytrivia/importer/ListFileParser.java
@@ -1,4 +1,4 @@
-package ponytrivia.importer;
+package ponytrivia.Importer;
 
 import java.io.BufferedReader;
 import java.io.EOFException;
@@ -12,72 +12,72 @@ import java.util.regex.Pattern;
 
 public class ListFileParser
 {
-	private FileInputStream fstream;
-	private BufferedReader reader;
-	
-	public ListFileParser(String filename) throws IOException 
-	{
-		fstream = new FileInputStream(filename);
-		reader = new BufferedReader(new InputStreamReader(fstream));
-	}
-	
-	public void close()
-	{
-		if (reader != null) {
-			try{
-				reader.close();
-			} catch (IOException ex) {
-			}
-		}
-		if (fstream != null) {
-			try {
-				fstream.close();
-			} catch (IOException ex) {
-			}
-		}
-		fstream = null;
-		reader = null;
-	}
-	
-	public String readLine() throws IOException
-	{
-		return reader.readLine();
-	}
+private FileInputStream fstream;
+private BufferedReader reader;
 
-	public void skipUntil(String regex) throws IOException
-	{
-		readUntil(regex);
-	}
-	
-	public List<String> readUntil(String regex) throws IOException
-	{
-		return readUntil(regex, false);
-	}
+public ListFileParser(String filename) throws IOException
+{
+fstream = new FileInputStream(filename);
+reader = new BufferedReader(new InputStreamReader(fstream));
+}
 
-	public List<String> readUntil(String regex, boolean includeLastLine) throws IOException
-	{
-		ArrayList<String> lines = new ArrayList<String>();
-		String line;
-		Pattern p = Pattern.compile(regex);
-		
-		while (true) {
-			line = readLine();
-			if (line == null) {
-				if (lines.isEmpty()) {
-					throw new EOFException("Not found");
-				}
-				break;
-			}
-			Matcher m = p.matcher(line);
-			if (m.matches()) {
-				if (includeLastLine) {
-					lines.add(line);
-				}
-				break;
-			}
-			lines.add(line);
-		}
-		return lines;
-	}
+public void close()
+{
+if (reader != null) {
+try{
+reader.close();
+} catch (IOException ex) {
+}
+}
+if (fstream != null) {
+try {
+fstream.close();
+} catch (IOException ex) {
+}
+}
+fstream = null;
+reader = null;
+}
+
+public String readLine() throws IOException
+{
+return reader.readLine();
+}
+
+public void skipUntil(String regex) throws IOException
+{
+readUntil(regex);
+}
+
+public List<String> readUntil(String regex) throws IOException
+{
+return readUntil(regex, false);
+}
+
+public List<String> readUntil(String regex, boolean includeLastLine) throws IOException
+{
+ArrayList<String> lines = new ArrayList<String>();
+String line;
+Pattern p = Pattern.compile(regex);
+
+while (true) {
+line = readLine();
+if (line == null) {
+if (lines.isEmpty()) {
+throw new EOFException("Not found");
+}
+break;
+}
+Matcher m = p.matcher(line);
+if (m.matches()) {
+if (includeLastLine) {
+lines.add(line);
+}
+break;
+}
+lines.add(line);
+}
+return lines;
+}
 
 }
