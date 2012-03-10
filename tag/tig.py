736d4e916950254b6a60ced88eefcae6d1862ad0
Tomer Filiba <tomerfiliba@gmail.com>
2012-03-10 23:39:23 +0200

    tig

--- tag/tig.py
+++ tag/tig.py
@@ -36,10 +36,9 @@ RIGHT_AUX = 2
 
 
 class TIG(object):
-    def __init__(self, start_symbol, init_trees, aux_trees):
+    def __init__(self, init_trees, aux_trees):
         self.init_trees = set(init_trees)
         self.aux_trees = set(aux_trees)
-        self.start_symbol = start_symbol
         
         for t in self.init_trees:
             if any(isinstance(n, Foot) for n in t.traverse_leaves()):
@@ -97,10 +96,10 @@ class State(object):
     def __ne__(self, other):
         return not (self == other)
     def __repr__(self):
-        prod = ["%s|" % (c,) if isinstance(c, NonTerminal) else str(c) 
+        prod = [u"%s\u2193" % (c,) if isinstance(c, NonTerminal) else str(c) 
             for c in self.tree.children]
-        prod.insert(self.dot, "@")
-        return "(%s->%s, %r:%r)" % (self.tree.root, " ".join(prod), self.i, self.j)
+        prod.insert(self.dot, u"\u00b7")
+        return u"(%s \u2192 %s, %r:%r)" % (self.tree.root, " ".join(prod), self.i, self.j)
     def next(self):
         if self.is_complete():
             return None
@@ -108,25 +107,31 @@ class State(object):
     def is_complete(self):
         return self.dot >= len(self.tree.children)
 
+import itertools
+
 class Chart(object):
     def __init__(self):
         self._elements = {}
         self._additions = []
+        self.counter = itertools.count()
     def __len__(self):
         return len(self._elements)
     def __iter__(self):
         return iter(self._elements)
     def __getitem__(self, key):
         return self._elements[key]
-    def add(self, state, prev):
-        self._additions.append((state, prev))
+    def add(self, state, prev = None, reason = None):
+        self._additions.append((state, prev, reason))
     def commit(self):
         prev_len = len(self._elements)
-        for s, prev in self._additions:
+        for s, prev, reason in self._additions:
             if s not in self._elements:
-                self._elements[s] = {prev}
+                s.index = self.counter.next()
+                self._elements[s] = {(prev, reason)}
             else:
-                self._elements[s].add(prev)
+                if s == prev:
+                    continue
+                self._elements[s].add((prev, reason))
         del self._additions[:]
         return prev_len != len(self._elements)
 
@@ -141,30 +146,30 @@ def handle_left_aux(grammar, chart, s):
     # Left Aux (2)
     for t in grammar.get_aux_trees(s.tree.root):
         if t.direction == LEFT_AUX:
-            chart.add(State(t, 0, s.j, s.j), s)
+            chart.add(State(t, 0, s.j, s.j), s, "LA2")
     
     # Left Aux (3)
     for s2 in chart:
         if (hasattr(s2.tree, "direction") and s.tree.root == s2.tree.root and 
                 s2.is_complete() and s2.i == s.j and s2.tree.direction == LEFT_AUX):
-            chart.add(State(s.tree, 0, s.i, s2.j), (s, s2))
+            chart.add(State(s.tree, 0, s.i, s2.j), s2, "LA3")
 
 def handle_scan(grammar, chart, s, tokens):
     if s.is_complete():
         return
     
-    if isinstance(s.next(), str): 
+    if isinstance(s.next(), str):
         # Scan (4)
-        if s.next() == tokens[s.j+1]:
-            chart.add(State(s.tree, s.dot+1, s.i, s.j+1), s)
+        if s.j + 1 < len(tokens) and s.next() == tokens[s.j+1]:
+            chart.add(State(s.tree, s.dot+1, s.i, s.j+1), s, "SC4")
         
         # Scan (5)
         if s.next() == "":
-            chart.add(State(s.tree, s.dot+1, s.i, s.j), s)
+            chart.add(State(s.tree, s.dot+1, s.i, s.j), s, "SC5")
 
     # Scan (6)
     if isinstance(s.next(), Foot):
-        chart.add(State(s.tree, s.dot+1, s.i, s.j), s)
+        chart.add(State(s.tree, s.dot+1, s.i, s.j), s, "SC6")
 
 def handle_substitution(grammar, chart, s):
     if s.is_complete() or not isinstance(s.next(), NonTerminal):
@@ -174,12 +179,12 @@ def handle_substitution(grammar, chart, s):
     
     # Substitution (7)
     for t in grammar.get_init_trees(r):
-        chart.add(State(t, 0, s.j, s.j), s)
+        chart.add(State(t, 0, s.j, s.j), s, "SU7")
 
     # Substitution (8)
     for s2 in chart:
         if s2.tree.root == r and s2.is_complete() and s2.i == s.j and s2.tree in grammar.init_trees:
-            chart.add(State(s.tree, s.dot + 1, s.i, s2.j), (s, s2))
+            chart.add(State(s.tree, s.dot + 1, s.i, s2.j), s2, "SU8")
 
 def handle_subtree(grammar, chart, s):
     if s.is_complete() or not isinstance(s.next(), Node):
@@ -188,12 +193,12 @@ def handle_subtree(grammar, chart, s):
     r = s.next()
     
     # Subtree (9)
-    chart.add(State(r, 0, s.j, s.j), s)
+    chart.add(State(r, 0, s.j, s.j), s, "ST9")
     
     # Subtree (10)
     for s2 in chart:
         if s2.tree == r and s2.is_complete() and s2.i == s.j:
-            chart.add(State(s.tree, s.dot+1, s.i, s2.j), (s, s2))
+            chart.add(State(s.tree, s.dot+1, s.i, s2.j), s2, "ST10")
 
 def handle_right_aux(grammar, chart, s):
     if not s.is_complete():
@@ -202,20 +207,20 @@ def handle_right_aux(grammar, chart, s):
     # Right Aux (11)
     for t in grammar.get_aux_trees(s.tree.root):
         if t.direction == RIGHT_AUX:
-            chart.add(State(t, 0, s.j, s.j), s)
+            chart.add(State(t, 0, s.j, s.j), s, "RA11")
 
     # Right Aux (12)
     for s2 in chart:
         if (hasattr(s2.tree, "direction") and s2.tree.root == s.tree.root and 
                 s2.is_complete() and s2.i == s.j and s2.tree.direction == RIGHT_AUX):
-            chart.add(State(s.tree, s.dot, s.i, s2.j), (s, s2))
+            chart.add(State(s.tree, s.dot, s.i, s2.j), s2, "RA12")
 
 
-def parse(grammar, tokens):
+def parse(start_symbol, grammar, tokens):
     chart = Chart()
     tokens = [None] + list(tokens)
-    for tree in grammar.get_init_trees(grammar.start_symbol):
-        chart.add(State(tree, 0, 0, 0), None)
+    for tree in grammar.get_init_trees(start_symbol):
+        chart.add(State(tree, 0, 0, 0), None, "IN1")
     
     while True:
         for s in chart:
@@ -231,10 +236,29 @@ def parse(grammar, tokens):
     
     matches = []
     for s in chart:
-        if (s.is_complete() and s.tree.root == grammar.start_symbol and s.i == 0 and 
+        if (s.is_complete() and s.tree.root == start_symbol and s.i == 0 and 
                 s.j == len(tokens) - 1 and s.tree in grammar.init_trees):
             matches.append(s)
     return matches, chart
+
+def print_chart(chart):
+    for s in sorted(chart, key = lambda s: s.index):
+        reasons = []
+        for s2, r in chart[s]:
+            reasons.append("%s:%s" % (getattr(s2, "index", ""), r))
+        
+        print "% 3d   % -30s    [%s]" % (s.index, s, ", ".join(reasons))
+
+
+class DevirationNode(object):
+    def __init__(self, name, children):
+        self.name = name
+        self.children = children
+    def __repr__(self):
+        return "(%s: %r)" % (self.name, self.children)
+
+def extract_tree(root, chart):
+    pass
 
 
 if __name__ == "__main__":
@@ -247,7 +271,7 @@ if __name__ == "__main__":
     Adv = NonTerminal("Adv")
     Adj = NonTerminal("Adj")
     
-    g = TIG(NP, 
+    g = TIG(
         init_trees = [
             NP("john"),
             NP("mary"),
@@ -263,26 +287,28 @@ if __name__ == "__main__":
             N(Adj("tasty"), Foot(N)),
         ],
     )
+
+#    matches, chart = parse(NP, g, "the tasty banana".split())
+#    print_chart(chart)
     
-    matches, chart = parse(g, "the banana".split())
+    T = NonTerminal("Term")
+    g2 = TIG([T("x"), T(T, "+", T)], [])
     
-    def f(s, chart, indent = 0):
+    matches, chart = parse(T, g2, "x + x + x".split())
+    print_chart(chart)
+    
+    def f(s, chart):
         if s is None:
             return
-        print "  " * indent + str(s)
-        if isinstance(s, tuple):
-            for s2 in chart[s[0]]:
-                f(s2, chart, indent + 1)
-            for s2 in chart[s[1]]:
-                f(s2, chart, indent + 1)
-        else:
-            for s2 in chart[s]:
-                f(s2, chart, indent + 1)
+        print s
+        for s2 in chart[s]:
+            s2 = s2[0]
+            f(s2, chart)
     
-    for s in matches:
-        f(s, chart)
-        
-
+    print "=============="
+    #f(matches[0], chart)
+    print matches
+    
 
 
 
